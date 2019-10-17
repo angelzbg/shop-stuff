1581105cd62980c2dec53251528a4e7d4aa8d3b1
angelzbg <badblo0dbg@gmail.com>
2019-10-17 14:44:15 +0300

    Updates

---
+++ precache-manifest.3e426f2eb737efb4b40efee2a542b413.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed4480fce70b91ab7accf169e9812b7",
+    "url": "/shop-stuff/index.html"
+  },
+  {
+    "revision": "0a4f235bb24713459218",
+    "url": "/shop-stuff/static/css/main.4fd918d6.chunk.css"
+  },
+  {
+    "revision": "36f324c257dae32a3def",
+    "url": "/shop-stuff/static/js/2.850ea480.chunk.js"
+  },
+  {
+    "revision": "0a4f235bb24713459218",
+    "url": "/shop-stuff/static/js/main.7a537ba3.chunk.js"
+  },
+  {
+    "revision": "1a422602680cc949cdcd",
+    "url": "/shop-stuff/static/js/runtime-main.daf48d0a.js"
+  },
+  {
+    "revision": "5c89599e282fc9d48ca4285b357f11bb",
+    "url": "/shop-stuff/static/media/res_6eb1b2fb27751ab2254d790a3fddb0d6_450x450_bfte.5c89599e.jpeg"
+  },
+  {
+    "revision": "01bc3c51c079fce7e3ae68d86d8b4da6",
+    "url": "/shop-stuff/static/media/res_7a38bd960822c1b2188874750cb3f577_450x450_1rum.01bc3c51.jpg"
+  },
+  {
+    "revision": "fc2c643b87b765ee2fa6a098d943d62d",
+    "url": "/shop-stuff/static/media/res_8c2a6eaf3c79cab5e883fd97a5b204e0_450x450_fmaa.fc2c643b.jpg"
+  },
+  {
+    "revision": "61bb51abb560917608e784dcc5699e88",
+    "url": "/shop-stuff/static/media/res_c64779826626e5a1382fe0cfe9158ab1_450x450_3t3j.61bb51ab.jpg"
+  },
+  {
+    "revision": "edc0dfbc428a53792788cb81ae78e03b",
+    "url": "/shop-stuff/static/media/res_e9445b4a2cbb9950002766047d96c5f4_450x450_am6o.edc0dfbc.jpg"
+  },
+  {
+    "revision": "3c6e017627cae27400812ecad9e55a96",
+    "url": "/shop-stuff/static/media/res_edb73b6e77be1aca39e2878dd2156ef2_450x450_mj1d.3c6e0176.jpg"
+  },
+  {
+    "revision": "f974bf47e71e20ef2ffecced85e9b9ae",
+    "url": "/shop-stuff/static/media/res_fc511f4dd86ac806e3500d0874766060_450x450_ea84.f974bf47.jpg"
+  }
+]);
